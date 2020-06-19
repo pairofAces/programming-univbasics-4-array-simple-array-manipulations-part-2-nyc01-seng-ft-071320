cedeca802492a1d4654e8658492131da7273707e
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-19 21:09:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,7 @@
 def using_concat(array1, array2)
-  @all_my_favs.concat(@my_favorite_things, more_favs)
+  @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["sports cars", "flatiron school"]
+    @all_my_favs = using_concat(@my_favorite_things, more_favs)
 end
 
   
